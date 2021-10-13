8a4c0b11cdedd6f1dbc451954879ffd78c3483bb
Seyeong <69945030+jsy2906@users.noreply.github.com>
2021-10-14 06:46:07 +0900

    model Update
    코드 함수화를 더 진행

--- model.py
+++ model.py
@@ -11,14 +11,19 @@ channel = 3
 autotune = tf.data.experimental.AUTOTUNE
 seed = 42
 
-path = './art_classification/'
+# 데이터 경로 지정
+path = 'C:/Users/pc/Desktop/20171787/AI/art_classification'
 
 train = os.path.join(path, 'train')
+test = os.path.join(path, 'test/0')
+
+# trainset, valset 설정
 trainset = tf.keras.preprocessing.image_dataset_from_directory(train, 
                                                            image_size=(weight, height),
                                                            validation_split=.3,
                                                            subset='training',
                                                            seed=seed, batch_size=10)
+labels = trainset.class_names
 trainset = trainset.cache().prefetch(autotune)
 
 valset = tf.keras.preprocessing.image_dataset_from_directory(train, 
@@ -28,11 +33,7 @@ valset = tf.keras.preprocessing.image_dataset_from_directory(train,
                                                            seed=seed, batch_size=10)
 valset = valset.cache().prefetch(autotune)
 
-test = os.path.join(path, 'test/0')
-test = os.listdir(test)
-test_df = pd.DataFrame(test, columns=['file name'])
-
-# 데이터 증강
+# 데이터 Augmentation
 augmentor = tf.keras.Sequential([
         tf.keras.layers.experimental.preprocessing.RandomFlip(input_shape = (weight, height, channel)),
         tf.keras.layers.experimental.preprocessing.RandomRotation(0.3),
@@ -59,24 +60,47 @@ model = tf.keras.Sequential([
 es = tf.keras.callbacks.EarlyStopping(monitor = 'loss', patience =30)
 
 model.compile(optimizer='adam', loss='SparseCategoricalCrossentropy', metrics=['acc'])
-model.fit(trainset, validation_data=valset, epochs=100, callbacks=es)
+history = model.fit(trainset, validation_data=valset, epochs=100, callbacks=es)
 
 # Accuracy와 Loss 그래프로 시각화
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs = range(len(acc))
+def graph_show(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(len(acc))
 
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-plt.title('Acc vs Val_Acc')
-plt.plot(epochs, acc, label = 'acc')
-plt.plot(epochs, val_acc, label = 'val_acc')
-plt.legend()
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.title('Acc vs Val_Acc')
+    plt.plot(epochs, acc, label = 'acc')
+    plt.plot(epochs, val_acc, label = 'val_acc')
+    plt.legend()
 
-plt.subplot(1, 2, 2)
-plt.title('Loss vs Val_Loss')
-plt.plot(epochs, loss, label = 'loss')
-plt.plot(epochs, val_loss, label = 'val_loss')
-plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.title('Loss vs Val_Loss')
+    plt.plot(epochs, loss, label = 'loss')
+    plt.plot(epochs, val_loss, label = 'val_loss')
+    plt.legend()
+
+# testset 설정
+test_dir = os.listdir(test)
+    
+# testset 시각화
+def im_show(file_name):
+    image_dir = os.path.join(test, file_name)
+    image = tf.image.decode_jpeg(tf.io.read_file(image_dir))
+    plt.imshow(image)
+
+# 결과 예측
+def pred(file_name):
+    image_dir = os.path.join(test, file_name)
+    image = tf.image.decode_jpeg(tf.io.read_file(image_dir))
+    arr = tf.keras.preprocessing.image.img_to_array(image)
+    arr_exp = tf.expand_dims(arr, axis = 0)
+    
+    prediction = model.predict(arr_exp)
+    score = tf.nn.softmax(prediction[0])
+    result = np.argmax(score)
+    
+    return result, labels[result]
